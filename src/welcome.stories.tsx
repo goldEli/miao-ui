ad0a72b2d96d217f3a51a16d94cd60dcf9749e5c
缪宇 <miaoyu@sefonsoft.com>
2020-10-09 15:39:53 +0800

    add welcome page story

---
+++ src/welcome.stories.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+
+storiesOf('Welcome page', module)
+  .add('welcome', () => {
+    return (
+      <>
+        <h1>欢迎来到 miao-ui 组件库</h1>
+        <p>miao-ui 基于 React 开发的一套组件库</p>
+        <h3>安装试试</h3>
+        <code>
+          npm install @miao-ui --save
+        </code>
+      </>
+    )
+  }, { info : { disable: true }})
