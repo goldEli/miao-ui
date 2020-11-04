63427cc53acdfdb67eedd4567d0d7ef3cbbad05f
miaoyu <mioayu2009@qq.com>
2020-11-04 17:19:01 +0800

    add menu test

---
+++ src/components/Menu/menu.test.tsx
@@ -0,0 +1,27 @@
+import { faItalic } from "@fortawesome/free-solid-svg-icons"
+import React from "react"
+import Menu from "./menu"
+
+describe("test Menu and MenuItem componnent in default(horizontal) mode", () => {
+  it('should render correct Menu and MenuItem baseed on default props', () => {
+
+  })
+  it("click items should change active and call the right callback", () => {
+
+  })
+  it('should show dropDown itmes when hover on subMenu', () => {
+
+  })
+})
+
+describe('test Menu and MenuItem component in vertical mode', () => {
+  it("should render vertical mode when mode is set to vertical", () => {
+
+  })
+  it("should show dropdown items click on subMenue for vertical mode", () => {
+
+  })
+  it("should show subMenu dropdown when defaultOpenSubMenus contains SubMenu index", () => {
+
+  })
+})
