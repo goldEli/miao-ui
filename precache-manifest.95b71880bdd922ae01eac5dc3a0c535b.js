f35a6c3a5412877312be1529bf315f67f73eb116
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-11-05 08:22:10 +0000

    Deploy goldEli/miao-ui to github.com/goldEli/miao-ui.git:gh-pages

---
+++ precache-manifest.95b71880bdd922ae01eac5dc3a0c535b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad1b8fa0ce935ebb80369fd683177bc7",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.bd4d8df51420c4f6e839.bundle.js"
+  },
+  {
+    "url": "runtime~main.bd4d8df51420c4f6e839.bundle.js"
+  },
+  {
+    "revision": "b7999566c31df3dee832",
+    "url": "static/css/main.304e8c36.chunk.css"
+  },
+  {
+    "revision": "5ae4a044b5ce00510cf83ca02f3512bd",
+    "url": "static/media/style.5ae4a044.css"
+  },
+  {
+    "url": "vendors~main.bd4d8df51420c4f6e839.bundle.js"
+  },
+  {
+    "url": "vendors~main.bd4d8df51420c4f6e839.bundle.js.LICENSE.txt"
+  }
+]);
