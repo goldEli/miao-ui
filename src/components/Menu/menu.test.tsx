dc8a6595b488e7fbffb20d15eab4b51a699b486b
miaoyu <miaoyu@sefonsoft.com>
2020-11-05 16:09:53 +0800

    add menu test

--- src/components/Menu/menu.test.tsx
+++ src/components/Menu/menu.test.tsx
@@ -1,27 +1,97 @@
-import { faItalic } from "@fortawesome/free-solid-svg-icons"
-import React from "react"
-import Menu from "./menu"
+import React from "react";
+import { render, RenderResult, fireEvent, wait } from "@testing-library/react";
+import Menu, { MenuProps } from "./menu";
+import MenuItem from "./menuItem";
+import SubMenu from "./subMenu";
+
+const generateMenu = (props: MenuProps) => {
+  return (
+    <Menu {...props}>
+      <MenuItem>active</MenuItem>
+      <MenuItem disabled>disabled</MenuItem>
+      <MenuItem>test</MenuItem>
+      <SubMenu title="dropMenu">
+        <MenuItem>drop1</MenuItem>
+      </SubMenu>
+    </Menu>
+  );
+};
+
+const testProps: MenuProps = {
+  defaultIndex: "0",
+  onSelect: jest.fn(),
+  className: "test",
+};
+
+const testVerProps: MenuProps = {
+  defaultIndex: "0",
+  mode: "vertical",
+};
+
+let wrapper: RenderResult;
+let wrapper2: RenderResult;
+let menuElement: HTMLElement;
+let activeElement: HTMLElement;
+let disabledElement: HTMLElement;
 
 describe("test Menu and MenuItem componnent in default(horizontal) mode", () => {
-  it('should render correct Menu and MenuItem baseed on default props', () => {
-
-  })
+  beforeEach(() => {
+    wrapper = render(generateMenu(testProps));
+    menuElement = wrapper.getByTestId("test-menu");
+    activeElement = wrapper.getByText("active");
+    disabledElement = wrapper.getByText("disabled");
+  });
+  it("should render correct Menu and MenuItem based on default props", () => {
+    expect(menuElement).toBeInTheDocument();
+    expect(menuElement).toHaveClass("test miao-menu");
+    expect(menuElement.querySelectorAll(":scope > li").length).toEqual(4);
+    expect(activeElement).toHaveClass("is-active");
+    expect(disabledElement).toHaveClass("is-disabled");
+  });
   it("click items should change active and call the right callback", () => {
+    const thirdItem = wrapper.getByText("test");
+    fireEvent.click(thirdItem);
 
+    expect(thirdItem).toHaveClass("is-active");
+    expect(activeElement).not.toHaveClass("is-active");
+    expect(testProps.onSelect).toHaveBeenCalledWith("2");
+    fireEvent.click(disabledElement);
+    expect(disabledElement).not.toHaveClass("is-active");
+    expect(testProps.onSelect).not.toHaveBeenCalledWith("1");
+  });
+  it("should show dropDown items when hover on subMenu", async () => {
+    expect(wrapper.queryByText("drop1")).toBeNull();
+    const dropMenuElement = wrapper.getByText("dropMenu");
+    fireEvent.mouseEnter(dropMenuElement);
+    await wait(() => {
+      expect(wrapper.getByText("drop1")).toBeInTheDocument();
+    });
+    const drop1Element = wrapper.getByText("drop1");
+    fireEvent.click(drop1Element);
+    expect(testProps.onSelect).toHaveBeenCalledWith("3-0") 
+
+    fireEvent.mouseLeave(dropMenuElement);
+    await wait(() => {
+      expect(wrapper.queryByText("drop1")).toBeNull();
+    })
+  });
+});
+
+describe("test Menu and MenuItem component in vertical mode", () => {
+  beforeEach(() => {
+    wrapper2 = render(generateMenu(testVerProps))
   })
-  it('should show dropDown itmes when hover on subMenu', () => {
-
-  })
-})
-
-describe('test Menu and MenuItem component in vertical mode', () => {
   it("should render vertical mode when mode is set to vertical", () => {
-
-  })
-  it("should show dropdown items click on subMenue for vertical mode", () => {
-
-  })
-  it("should show subMenu dropdown when defaultOpenSubMenus contains SubMenu index", () => {
-
-  })
-})
+    const menuElement = wrapper2.getByTestId("test-menu")
+    expect(menuElement).toBeInTheDocument()
+    expect(menuElement).toHaveClass("miao-menu-vectical")
+  });
+  it("should show dropdown items click on subMenue for vertical mode", async () => {
+    expect(wrapper2.queryByText("drop1")).toBeNull();
+    const dropMenuElement = wrapper2.getByText("dropMenu");
+    fireEvent.click(dropMenuElement);
+    await wait(() => {
+      expect(wrapper2.getByText("drop1")).toBeInTheDocument();
+    });
+  });
+});
--- src/components/Menu/menu.tsx
+++ src/components/Menu/menu.tsx
@@ -1,71 +1,72 @@
-import React from "react"
+import React from "react";
 import { MenuItemProps } from "./menuItem";
 import classNames from "classnames";
 
-type MenuMode = "horizontal" | "vertical"
+type MenuMode = "horizontal" | "vertical";
 export interface MenuProps {
-    defaultIndex?: string
-    mode?: MenuMode
+  defaultIndex?: string;
+  mode?: MenuMode;
+  onSelect?: (index: string) => void;
+  className?: string;
 }
 
 interface MenuContext {
-    activeIndex: string;
-    onChangeActiveIndex?: (index: string) => void;
-    mode: MenuMode;
+  activeIndex: string;
+  onChangeActiveIndex?: (index: string) => void;
+  mode: MenuMode;
 }
 
 export const menuContext = React.createContext<MenuContext>({
-    activeIndex: "0",
-    mode: "horizontal"
-})
+  activeIndex: "0",
+  mode: "horizontal",
+});
 
-const Menu: React.FC<MenuProps> = props => {
+const Menu: React.FC<MenuProps> = (props) => {
+  const [activeIndex, setActiveIndex] = React.useState(
+    props.defaultIndex as string
+  );
 
-    const [activeIndex, setActiveIndex] = React.useState(props.defaultIndex as string)
+  const onChangeActiveIndex = (index: string) => {
+    setActiveIndex(index);
+    props?.onSelect?.(index);
+  };
 
-    const onChangeActiveIndex = (index: string) => {
-        setActiveIndex(index)
-    }
+  const renderChildren = () => {
+    return React.Children.map(props.children, (child, index) => {
+      const ele = child as React.FunctionComponentElement<MenuItemProps>;
+      const { displayName } = ele.type;
+      if (displayName === "MenuItem" || displayName === "SubMenu") {
+        return React.cloneElement(ele, {
+          index: index.toString(),
+        });
+      }
+      console.error("warning: Menu has a child which is not a MenuItem");
+    });
+  };
 
-    const renderChildren = () => {
-        return React.Children.map(props.children, (child, index) => {
-            const ele = child as React.FunctionComponentElement<MenuItemProps>
-            const { displayName } = ele.type
-            if (displayName === "MenuItem" || displayName === "SubMenu") {
-                return React.cloneElement(ele, {
-                    index: index.toString()
-                })
-            }
-            console.error("warning: Menu has a child which is not a MenuItem")
+  const contextProps = {
+    activeIndex: activeIndex,
+    onChangeActiveIndex,
+    mode: props.mode as MenuMode,
+  };
 
-        })
-    }
+  const classes = classNames(props.className, "miao-menu", {
+    "miao-menu-horizontal": props.mode === "horizontal",
+    "miao-menu-vectical": props.mode === "vertical",
+  });
 
-    const contextProps = {
-        activeIndex: activeIndex,
-        onChangeActiveIndex,
-        mode: props.mode as MenuMode,
-    }
-
-    const classes = classNames(
-        "miao-menu",
-        {
-            "miao-menu-horizontal": props.mode === "horizontal",
-            "miao-menu-vectical": props.mode === "vertical"
-        },
-    )
-
-    return <ul className={classes}>
-        <menuContext.Provider value={contextProps}>
-            {renderChildren()}
-        </menuContext.Provider>
-
+  return (
+    <ul data-testid="test-menu" className={classes}>
+      <menuContext.Provider value={contextProps}>
+        {renderChildren()}
+      </menuContext.Provider>
     </ul>
-}
+  );
+};
 
 Menu.defaultProps = {
-    defaultIndex: "0",
-    mode: "horizontal"
-}
+  defaultIndex: "0",
+  mode: "horizontal",
+};
 
-export default Menu
+export default Menu;
